0f7b5f5760c7805908eb60b890812531e37890eb
bertrand <bertrand.maubert@ch-lepuy.fr>
2024-04-09 09:39:26 +0200

    ajout d'une classe.

--- essais/essai_pyautogui.py
+++ essais/essai_pyautogui.py
@@ -3,13 +3,27 @@ import pyperclip
 
 from time import sleep
 
-def find_position():
-    sleep(2)
-    return pyautogui.position()
 
-def send_to_target(msg, pos):
-    pyautogui.click(pos[0], pos[1])
-    sleep(1)
-    pyperclip.copy(msg)
-    pyautogui.hotkey('ctrl', 'v')
+
+class GuiApp:
+    def __init__(self):
+        self.cible = self.find_position()
+
+    def find_position(self):
+        print("Cliquer sur la zone à enregistrer")
+        sleep(2)
+        pos = pyautogui.position()
+        print(pos)
+        return pyautogui.position()
+
+
+    def send_to_target(self, msg,):
+        pyautogui.click(self.cible[0], self.cible[1])
+        sleep(1)
+        pyperclip.copy(msg)
+        pyautogui.hotkey('ctrl', 'v')
+
+if __name__ == '__main__':
+
+    app = GuiApp()
 
