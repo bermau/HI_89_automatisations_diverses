781f5e1cc0dd258089647592e080190673fa2319
bertrand <bertrand.maubert@ch-lepuy.fr>
2024-05-24 11:10:11 +0200

    bug_fix : il fallait refaire l'image à chercher

--- essais/rejouer_des_cr.py
+++ essais/rejouer_des_cr.py
@@ -10,33 +10,34 @@ import pyperclip
 from time import sleep
 from pprint import pprint
 
+lst_of_files = ['24041234', '24041227', '24040301']
 
-lst_of_files =  [ '24041234',  '24041227', '24040301']
+glm_app = {'icone_liste_patients': Point(x=39, y=82),
+           'liste_patients': Point(x=773, y=313),
+           'bas_crs': Point(x=997, y=214)
+           }
 
-
-glm_app = {'icone_liste_patients' : Point(x=39, y=82),
-           'liste_patients' : Point(x=773, y=313),
-            'bas_crs' : Point(x=997, y=214)
-             }
 
 def ouvrir_liste_patients():
-    pyautogui.moveTo(glm_app['icone_liste_patients'], duration = 0.5)
+    pyautogui.moveTo(glm_app['icone_liste_patients'], duration=0.5)
     pyautogui.click()
     sleep(0.8)
     pyautogui.moveTo(glm_app['liste_patients'], duration=0.5)
     pyautogui.click()
 
-def find_order(order_id : str) -> None:
+
+def find_order(order_id: str) -> None:
     """
     Trouve un dossier.
     Préalable : la liste des dossiers doit être ouverte.
     :param order_id: str :
     :return:
     """
-    pyautogui.hotkey('ctrl', 'f') # Find
+    pyautogui.hotkey('ctrl', 'f')  # Find
     pyautogui.write(order_id)
     pyautogui.write(["tab", 'tab', 'Enter'])
     sleep(0.8)
+
 
 def open_cr():
     pyautogui.write(['alt', 'D'])
@@ -44,21 +45,19 @@ def open_cr():
     # A ce point la liste des CR est ouverte.
     sleep(0.5)
 
+
 def find_crs_locations():
     """Fonction pour contourner un bug de pyautogui"""
-    pyautogui.useImageNotFoundException(True)
     pyautogui.write(['up', 'up'])
     sleep(0.5)
-    locations = pyautogui.locateAllOnScreen("../images/icone_1_black_on_white.png")
-    lst_loc = list(locations)
-    # if not lst_loc:
-    #     # On peut réessayer en déplaçant la ligne sélectionnée
-    #     pyautogui.write(['up', 'up'])
-    #     sleep(0.2)
-    #     locations = pyautogui.locateAllOnScreen("../images/icone_1_black_on_white.png")
-    #     lst_loc = list(locations)
 
-    return lst_loc
+    pyautogui.useImageNotFoundException(False)
+    locations = pyautogui.locateAllOnScreen("../images/cyber_prod_black_on_white.png")
+    if locations:
+        return list(locations)
+    else:
+        return None
+
 
 def locate_all_cr():
     sleep(0.5)
@@ -69,7 +68,6 @@ def locate_all_cr():
     sleep(0.2)
 
     cr_location = find_crs_locations()
-
 
     if not cr_location:
         print("Je n'ai pas trouvé d'image de CR")
@@ -83,14 +81,16 @@ def locate_all_cr():
         pyautogui.click()
         rejouer_cr()
 
-    sleep(0.5)
+    sleep(2)
+    pyautogui.write(['esc'])
+    sleep(0.2)
 
 
 def rejouer_cr():
     pyautogui.write(['f6'])
-    pyautogui.write(['tab', 'tab', 'tab', 'space'], interval = 0.1)
+    pyautogui.write(['tab', 'tab', 'tab', 'space'], interval=0.1)
     sleep(0.2)
-    ok_button= pyautogui.locateOnScreen("../images/OK_icon.png")
+    ok_button = pyautogui.locateOnScreen("../images/OK_icon.png")
     if ok_button:
         pyautogui.moveTo(ok_button)
         sleep(0.2)
@@ -98,18 +98,13 @@ def rejouer_cr():
 
 
 if __name__ == "__main__":
-    lst = """24059469
-24061010
-24059842
-24059983
-24059313
+    lst = """24059313
 24059042
 24059113
 24059204
 24059231"""
 
     for dossier in lst.split("\n"):
-
         print(f"Traitement du dossier {dossier}")
         # ouvrir_liste_patients()
         pyautogui.moveTo(glm_app['liste_patients'], duration=0.5)
@@ -118,4 +113,4 @@ if __name__ == "__main__":
         open_cr()
         locate_all_cr()
 
-        input("Dossier suivant...")
+        # input("Dossier suivant...")
