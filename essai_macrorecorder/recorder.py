ad39700d23700c66a5d5f5beaee268d7d4cef09a
bertrand <bertrand.maubert@ch-lepuy.fr>
2024-06-10 17:28:54 +0200

    recording ne sert à rien.

--- essai_macrorecorder/recorder.py
+++ essai_macrorecorder/recorder.py
@@ -18,28 +18,19 @@ def get_elapsed_time():
     return int((time.perf_counter() - start_time) * 1000)
 
 def on_move(x, y):
-
-    if recording:
-
-        actions.append(('move', x, y,  get_elapsed_time()))
+    actions.append(('move', x, y,  get_elapsed_time()))
 
 def on_click(x, y, button, pressed):
-
-    if recording:
-        actions.append(('click', x, y, button.name, pressed, get_elapsed_time()))
+    actions.append(('click', x, y, button.name, pressed, get_elapsed_time()))
 
 def on_scroll(x, y, dx, dy):
-
-    if recording:
-        actions.append(('scroll', x, y, dx, dy, get_elapsed_time()))
+    actions.append(('scroll', x, y, dx, dy, get_elapsed_time()))
 
 def on_press(key):
-
-    if recording:
-        try:
-            actions.append(('press', key.char, get_elapsed_time()))
-        except AttributeError:
-            actions.append(('press', key.name, get_elapsed_time())) # ???? idem
+    try:
+        actions.append(('press', key.char, get_elapsed_time()))
+    except AttributeError:
+        actions.append(('press', key.name, get_elapsed_time())) # ???? idem
 
 def on_release(key):
     try:
@@ -47,15 +38,14 @@ def on_release(key):
     except AttributeError:
         actions.append(('release', key.char, get_elapsed_time()))
     if key == Key.esc:
-        # Stop listener
-        print("Stop !")
-        print(actions)
-        recording = False
-        mouse_listener.stop()
-        return False
+            # Stop listener
+            print("Stop !")
+            print(actions)
+            mouse_listener.stop()
+            return False
+
 
 time.sleep(4)
-
 
 # Start listeners
 with MouseListener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as mouse_listener, \
